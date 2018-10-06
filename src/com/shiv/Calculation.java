c022976c536c7db154708189ca015896032988d1
shivprogrammer <shiv.unadkat@gmail.com>
2018-10-06 00:27:33 -0700

    built out Calculation abstract class

--- src/com/shiv/Calculation.java
+++ src/com/shiv/Calculation.java
@@ -5,5 +5,13 @@ public abstract class Calculation {
     private final String secondNum;
     private final String operation;
     protected String result;
-    
+
+    public Calculation(String firstNum, String secondNum, String operation) {
+        this.firstNum = firstNum;
+        this.secondNum = secondNum;
+        this.operation = operation;
+        this.result = calculateResult();
+    }
+
+    public abstract String calculateResult();
 }
